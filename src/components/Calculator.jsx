e394b0a89550afecd10d97d25c4949672e3b2508
Davi Fernandes <davi.fer159@gmail.com>
2024-10-08 10:02:36 -0300

    version 1 completed

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -2,12 +2,99 @@ import { useState } from "react";
 import "./Calculator.css";
 
 const Calculator = () => {
+  const [currentValue, setCurrentValue] = useState("0");
+  const [pendingOperation, setPendingOperation] = useState(null);
+  const [pendingValue, setPendingValue] = useState(null);
+  const [completeOperation, setCompleteOperation] = useState("");
+
+  const keyPadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const operations = ["+", "-", "*", "/"];
+
+  const handleClick = (val) => {
+    setCurrentValue((prevValue) => {
+      if (prevValue === "0") {
+        return val;
+      } else {
+        return prevValue + val;
+      }
+    });
+
+    setCompleteOperation((prevOperation) => prevOperation + val);
+  };
+
+  const handleClear = () => {
+    setCurrentValue("0");
+    setPendingOperation(null);
+    setPendingValue(null);
+    setCompleteOperation("");
+  };
+
+  const handleOperation = (operation) => {
+    setCompleteOperation(currentValue + " " + operation);
+    setPendingOperation(operation);
+    setPendingValue(currentValue);
+    setCurrentValue("0");
+  };
+
+  const handleCalculate = () => {
+    if (!pendingOperation || !pendingValue) {
+      return;
+    }
+
+    const num1 = Number(pendingValue);
+    const num2 = Number(currentValue);
+
+    let result;
+
+    switch (pendingOperation) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        if (num2 !== 0) {
+          result = num1 + num2;
+        } else {
+          setCurrentValue("Error");
+          setCompleteOperation("Error");
+          setPendingOperation(null);
+          setPendingValue(null);
+          return;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+    setCompleteOperation(
+      `${pendingValue} ${pendingOperation} ${currentValue} = ${result}`
+    );
+  };
+
   return (
     <div className="calculator">
-      <div className="complete-operation">3 + 3 = 6</div>
-      <div className="display">6</div>
-      <div className="buttons"></div>
-      <button>=</button>
+      <div className="complete-operation">{completeOperation}</div>
+      <div className="display">{currentValue}</div>
+      <div className="buttons">
+        <button onClick={handleClear}>AC</button>
+        {keyPadNumbers.map((num) => (
+          <button key={num} onClick={() => handleClick(num)}>
+            {num}
+          </button>
+        ))}
+        {operations.map((operation) => (
+          <button key={operation} onClick={() => handleOperation(operation)}>
+            {operation}
+          </button>
+        ))}
+        <button onClick={handleCalculate}>=</button>
+      </div>
     </div>
   );
 };
