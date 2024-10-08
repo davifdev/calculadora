e09ae8cd342ee030f37aa9ec3e7e6457d4d6483f
Davi Fernandes <davi.fer159@gmail.com>
2024-10-08 08:53:26 -0300

    initial setup

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./Calculator.css";
+
+const Calculator = () => {
+  return (
+    <div className="calculator">
+      <div className="complete-operation">3 + 3 = 6</div>
+      <div className="display">6</div>
+      <div className="buttons"></div>
+      <button>=</button>
+    </div>
+  );
+};
+
+export default Calculator;
